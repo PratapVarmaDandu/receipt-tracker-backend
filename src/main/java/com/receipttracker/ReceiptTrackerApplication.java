1725760984f45cd1f58232e74f0a9881cb6d100c
pdandu2 <pdandu@kent.edu>
2026-05-19 23:20:42 -0400

    Update ReceiptTrackerApplication.java

--- src/main/java/com/receipttracker/ReceiptTrackerApplication.java
+++ src/main/java/com/receipttracker/ReceiptTrackerApplication.java
@@ -6,6 +6,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ReceiptTrackerApplication {
     public static void main(String[] args) {
+        String url  = System.getenv("PROD_DB_URL");
+        String user = System.getenv("PROD_DB_USER");
+        String pass = System.getenv("PROD_DB_PASSWORD");
+        System.out.println("=== DB DEBUG ===");
+        System.out.println("PROD_DB_URL      = " + url);
+        System.out.println("PROD_DB_USER     = " + user);
+        System.out.println("PROD_DB_PASSWORD = " + pass);
+        System.out.println("Full connection  = DriverManager.getConnection(\"" + url + "\", \"" + user + "\", \"" + pass + "\")");
+        System.out.println("================");
         SpringApplication.run(ReceiptTrackerApplication.class, args);
     }
 }
